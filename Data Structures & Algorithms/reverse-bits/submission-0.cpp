f74405bf2ea2bce56bcd5a6e92641a6a25cc3c9f
BHANU PRAKASH BHARDWAJ <125625810+Gallant-Bhanu-Bhardwaj@users.noreply.github.com>
2026-04-14 18:41:34 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int num = 0;
+        for(int i=0;i<32;i++)
+        {
+             num <<= 1;          
+            num |= (n & 1);    
+            n >>= 1;  
+        }
+        return num;
+    }
+};
